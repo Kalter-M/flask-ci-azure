01ab795cfb36fe7fd73ba7f378c0890c7cf0ee0b
Alice <kapranchikova_al@mail.ru>
2019-04-14 22:53:00 +0300

    add simple crud. rename files

--- tests.py
+++ tests.py
@@ -1,8 +1,16 @@
-from hello import app
+import json
+
+from simpleapi import app
 with app.test_client() as c:
     response = c.get('/')
-    assert response.data == b'Hello World!'
+    assert response.data != b''
     assert response.status_code == 200
+
+    response = c.get('/1')
+    assert response.data == b'value1'
+    assert response.status_code == 200
+
+    # response = c.post('/', data=json.dumps({'val': 'valueeee45', 'id': '7'}))
 
     response = c.get('/long')
     assert response.data == b'end long'
